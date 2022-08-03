a7c55570ef30230ac8d43989eb24dc2ae33471a1
yusuf.boyaci <yusuf.boyaci@agarthateknoloji.com.tr>
2022-08-03 15:23:15 +0300

    ----

--- InputMask/Controllers/DevExpressController.cs
+++ InputMask/Controllers/DevExpressController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InputMask.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,16 @@ namespace InputMask.Controllers
 {
     public class DevExpressController : Controller
     {
-        public IActionResult Index() => View();
-        public IActionResult DevExpressControlIndex()
+        private readonly Context db;
+        public DevExpressController(Context context)
         {
+            db = context;
+        }
+        public IActionResult Index() => View();
+        public IActionResult DevExpressControlIndex(LamiData lamiData)
+        {
+            db.Datas.Add(lamiData);
+            db.SaveChanges();
             return View();
         }
     }
--- InputMask/Models/LamiData.cs
+++ InputMask/Models/LamiData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace InputMask.Models
 {
     public class LamiData
     {
+        [Key]
         public Guid Id { get; set; }
         public decimal Number1 { get; set; }
     }
